f6c8b68432576ec4b3e25191e5efeadb382af145
黄涛 <1198787527@qq.com>
2021-06-24 20:52:14 +0800

    commit cartControl

--- src/components/Food/Food.vue
+++ src/components/Food/Food.vue
@@ -56,10 +56,13 @@
     bottom: 48px
     z-index: 101
     width: 100%
+
     &.fade-enter-active, &.fade-leave-active
       transition opacity .5s
+
     &.fade-enter, &.fade-leave-to
       opacity 0
+
     .food-content
       position absolute
       left 50%
@@ -70,17 +73,20 @@
       z-index 66
       background #fff
       border-radius 5px
+
       .image-header
         position: relative
         width: 100%
         height: 0
         padding-top: 100%
+
         img
           position: absolute
           top: 0
           left: 0
           width: 100%
           height: 100%
+
         .foodpanel-desc
           font-size 10px
           color #ddd
@@ -90,49 +96,61 @@
           left 0
           right 0
           padding 0 10px 10px
+
         .back
           position: absolute
           top: 10px
           left: 0
+
           .icon-arrow_left
             display: block
             padding: 10px
             font-size: 20px
             color: #fff
+
       .content
         position: relative
         padding: 18px
+
         .title
           line-height: 14px
           margin-bottom: 8px
           font-size: 14px
           font-weight: 700
           color: rgb(7, 17, 27)
+
         .detail
           margin-bottom: 18px
           line-height: 10px
           height: 10px
           font-size: 0
+
           .sell-count, .rating
             font-size: 10px
             color: rgb(147, 153, 159)
+
           .sell-count
             margin-right: 12px
+
         .price
           font-weight: 700
           line-height: 24px
+
           .now
             margin-right: 8px
             font-size: 14px
             color: rgb(240, 20, 20)
+
           .old
             text-decoration: line-through
             font-size: 10px
             color: rgb(147, 153, 159)
+
         .cartcontrol-wrapper
           position: absolute
           right: 12px
           bottom: 12px
+
         .buy
           position: absolute
           right: 18px
@@ -146,11 +164,14 @@
           font-size: 10px
           color: #fff
           background: rgb(0, 160, 220)
+
           &.fade-transition
             transition: all 0.2s
             opacity: 1
+
           &.fade-enter, &.fade-leave
             opacity: 0
+
     .food-cover
       position absolute
       top 0
