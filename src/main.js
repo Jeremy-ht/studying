a4531e35c338b4f620bd6d6f124e12169e135af9
Jeremy <1198787527@qq.com>
2021-07-01 10:28:04 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App'
 import router from './router'
 import store from './store'
 import {Button} from 'mint-ui'
-
+import VueLazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
+import './filters'
 import './mock/mockServer'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading
+})
 
 Vue.component(Button.name, Button)
 
