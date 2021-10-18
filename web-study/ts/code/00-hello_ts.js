b651d457b7e65881178445d6b5aa3eb4494db4cb
黄涛 <Szhtjykj666>
2021-10-18 09:52:04 +0800

    添加typescript文档

---
+++ web-study/ts/code/00-hello_ts.js
@@ -0,0 +1,19 @@
+console.log('Hello TS');
+var a;
+a = 10;
+function sum(a, b) {
+    return a + b;
+}
+var number = sum(111, 123);
+var b;
+b = 123;
+b = true;
+var c;
+c = true;
+c = b;
+c = b;
+if (typeof b === "boolean") {
+    c = b;
+}
+var e;
+console.log(typeof e);
