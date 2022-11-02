219473a9b21af6383d98085c8e17eca51cc37156
黄涛 <Szhtjykj666>
2022-11-02 15:46:52 +0800

    add react - life cycle

--- web-study/react/preview.js
+++ web-study/react/preview.js
@@ -8,4 +8,67 @@
  * 5、高阶函数
  * 6、柯里化
  *
+ * 7、diff算法
+ * 8、key的作用
+ *
+ * 9、react 组件中通信
+ *    - 父传子
+ *    - 子传父
+ *    - 兄弟组件
+ *    （vue中又是如何传递的？）
+ *
+ * 10、axios
+ * 11、promise
+ *
+ *
+ *
+ *
+ *
+ *
+ * 01 - 邂逅向往的react
+ *      - 为什么要学习react
+ *      - react对于前端的重要性
+ *          - react的start数
+ *          - npm下载量
+ *          - 大中厂招聘
+ *      - 举例：美团、QQ音乐、知乎
+ *      - 学习路线
+ *      - react相关参考文档
+ *
+ * 02 - JSX & Virtual DOM?是什么
+ *      - 引入两个核心库
+ *          - React.js
+ *          - React-dom.js
+ *      - 解释jsx
+ *      - 案例
+ *
+ * 03 - react组件化
+ *      - 组件、模块化以及区别
+ *      - react两种组件
+ *          - 函数式组件
+ *          - 类式组件
+ *      - https://zh-hans.reactjs.org/docs/react-component.html
+ *
+ * 04 - react中的state（组件核心属性）
+ *      - 基本概念、基本使用
+ *      - 例子
+ *
+ * 05 - react中的props（组件核心属性）
+ *      - 基本概念、基本使用
+ *      - 例子
+ *
+ * 06 - react中的refs（组件核心属性）
+ *      - 基本概念、基本使用
+ *      - 例子
+ *
+ * 07 - 事件处理
+ *
+ * 08 - react组件生命周期
+ *      - 老的生命周期
+ *      - 新的生命周期
+ *      - 使用最多的
+ *          - componentDidMount
+ *          - componentWillUnmount
+ *          - render
+ *
  */
